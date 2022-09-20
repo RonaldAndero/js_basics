7f80e9fdaa1aaa59d33461e1d22ef2f272526499
Ronald Andero Õisman <ronald-andero.oisman@khk.ee>
2022-09-20 01:17:19 -0700

    vars

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+// vars
+let name = "Ronald Andero"
+
+// const
+const surname = "Õisman"
+
+// arrays
+const numbers = [1, 2, 3, 4, 5]
+
+// objects
+const person = {
+    firstname: "Ronald Andero",
+    surname: "Õisman"
+}
