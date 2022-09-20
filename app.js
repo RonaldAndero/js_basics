3b20433efbc6f7aa673cbc8d5c08ce5d15e23882
Ronald Andero Õisman <ronald-andero.oisman@khk.ee>
2022-09-20 01:28:40 -0700

    vars

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
+/*
 // vars
 let name = "Ronald Andero"
 
 // const
 const surname = "Õisman"
 
-// arrays
-const numbers = [1, 2, 3, 4, 5]
+*/
 
-// objects
+
+/*// objects
 const person = {
     firstname: "Ronald Andero",
     surname: "Õisman"
 }
+*!/*/
+
+// data types
+// string
+let name = "Ronald Andero"
+console.log(name)
+console.log(typeof name)
+
+let goodMark = 5
+console.log(goodMark)
+console.log(typeof goodMark)
+
+let persent = 50.3
+console.log(persent)
+console.log(typeof persent)
+
+let isHere = true
+console.log(isHere)
+console.log(typeof isHere)
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+console.log(typeof numbers)
+
+const person = {
+    firstname: "Ronald Andero",
+    surname: "Õisman"
+}
+console.log(person)
+console.log(typeof person)
+
+// null
+const airplane = null
+console.log(airplane)
+console.log(typeof airplane)
+
+// symbol
+const sym = Symbol()
+console.log(sym)
+console.log(typeof sym)
+
+// undefined
+let test
+console.log(test)
+console.log(typeof test)
