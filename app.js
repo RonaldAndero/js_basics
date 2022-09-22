93bd8a6af705dcb73ba9de25d01c4a2112011e3e
Ronald Andero Õisman <ronald-andero.oisman@khk.ee>
2022-09-22 03:05:55 -0700

    objects and attributes

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-const numbers1 = [43, 3, 75, 7, 99]
-const numbers2 = new Array(25, 5, 64, 77)
+const person = {
+    firstname: "Ronald",
+    surname: "Andero",
+    age: 18,
+    mail: "ronald.andero@gmail.com",
+    hobbies: ["tennis", "basketball"],
+    address: {
+        city: "Tartu",
+        county: "Tartumaa"
+    },
+    getBirthyear: function(){
+        return 2022 - this.age
+    }
+}
 
 let val
-
-val = numbers1.length
-val = numbers2.length
-
-val = numbers1[1]
-val = numbers1.indexOf(7)
-
-val = Array.isArray(numbers1)
-
-
-numbers1.push(250)
-numbers1.pop()
-numbers1.unshift(120)
-numbers1.shift()
-
-val = numbers1.concat(numbers2)
-
-const fruits = ["banana", "apple", "orange"]
-val = fruits.sort()
-
-val = numbers1.sort(function (x, y) {
-    return y - x
-})
-
+val = person.address.city
+val = person["surname"]
+val = person['hobbies'][1]
+val = person.address['city']
+val = person.getBirthyear()
 console.log(val)
+
+for(let i = 0; i < person.hobbies.length; i++){
+    console.log(person.hobbies[i])
+}
+
+/*person.hobbies.forEach(function (hobbie, index,hobbies){
+    console.log(hobbie + " on element indeksiga " + index)
+    console.log(hobbies)
+})*/
+
+person.hobbies.forEach((hobbie, index) => {
+    console.log(hobbie + " on element indeksiga " + index)
+})
