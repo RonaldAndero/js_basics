1d790feb3a6d72acfd0dbef4890c8efcef276fab
Ronald Andero Õisman <ronald-andero.oisman@khk.ee>
2022-09-22 03:56:20 -0700

    loops

--- app.js
+++ app.js
@@ -1,50 +1,38 @@
-/*
-if(condition) {
-    //if statements
-} else {
-    // else statements
+console.log('for loop')
+for (let i = 0; i < 10; i++) {
+    if(i == 5){
+        continue
+    }
+    console.log(i)
 }
 
- == equal -> ===
- != not equal -> !==
- */
+console.log('while')
 
-let personID;
-
-if(typeof personID === 'undefined'){
-    console.log("id is not correct")
-} else{
-    console.log('id is ${personID}')
+let number1 = 0;
+while (number1 < 10){
+    console.log(number1)
+    number1++
 }
 
-console.log(personID)
+console.log('do-while')
 
+let number2 = 0;
+do{
+    console.log(number2)
+    number2++
+} while(number2 < 10)
 
-const color = 'green'
-if(color === 'red'){
-    console.log('Stop')
-} else if(color === 'yellow'){
-    console.log('Attension')
-} else if(color === 'green') {
-    console.log('Go')
-} else {
-    console.log('not allowed')
+const cars = ['audi', 'tesla', 'bmw']
+cars.forEach((car) => {
+    console.log(cars)
+})
+
+const person = {
+    firstname: 'ronald',
+    lastname: 'andero',
+    age: 18
+}
+for (const personKey in person) {
+    console.log(person[personKey])
 }
 
-/*
-condition1 && condition2 - and
-condition1 // condition2 - or
- */
-
-const name = 'kadi'
-const age = 40
-
-if(age > 0 && age <= 12){
-    console.log('${name} on laps')
-} else if(age > 12 && age < 18){
-    console.log('${name} on nooruk')
-} else {
-    console.log('${name} on nooruk')
-}
-
-// ? - ternary
