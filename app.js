c6f506821598a5ed4231748cf6cec809ab70086a
Ronald Andero Õisman <ronald-andero.oisman@khk.ee>
2022-09-22 03:17:26 -0700

    Date

--- app.js
+++ app.js
@@ -1,35 +1,15 @@
-const person = {
-    firstname: "Ronald",
-    surname: "Andero",
-    age: 18,
-    mail: "ronald.andero@gmail.com",
-    hobbies: ["tennis", "basketball"],
-    address: {
-        city: "Tartu",
-        county: "Tartumaa"
-    },
-    getBirthyear: function(){
-        return 2022 - this.age
-    }
-}
+const today = new Date()
 
 let val
-val = person.address.city
-val = person["surname"]
-val = person['hobbies'][1]
-val = person.address['city']
-val = person.getBirthyear()
+val = today
+val = today.getMonth()
+val = today.getDay()
+val = today.getFullYear()
+
+val = new Date("09/22/2021")
+val = new Date("September 23 2022")
+
+val = new Date()
+val.setDate(23)
+
 console.log(val)
-
-for(let i = 0; i < person.hobbies.length; i++){
-    console.log(person.hobbies[i])
-}
-
-/*person.hobbies.forEach(function (hobbie, index,hobbies){
-    console.log(hobbie + " on element indeksiga " + index)
-    console.log(hobbies)
-})*/
-
-person.hobbies.forEach((hobbie, index) => {
-    console.log(hobbie + " on element indeksiga " + index)
-})
