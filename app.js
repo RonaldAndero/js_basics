4c18dbb67ee2b6718558572ca85cb7ae8a9a82ab
Ronald Andero Õisman <ronald-andero.oisman@khk.ee>
2022-09-26 23:54:41 -0700

    DOM element creation

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
-let val;
+const li = document.createElement('li')
+li.className = 'collection-item'
+let text = document.createTextNode('Study element creation')
+li.appendChild(text)
+
+const link = document.createElement('a')
+link.className = 'blue-text secondary-content'
+text = document.createTextNode('X')
+link.append(text)
+link.setAttribute('href', '#')
+
+li.appendChild(link)
 
 const list = document.querySelector('ul')
-const listItem = document.querySelector('li')
+list.appendChild(li)
 
-val = list
-val = list.childNodes
-val = list.childNodes[0].nodeName
-val = list.childNodes[0].nodeType
-val = list.childNodes[1].nodeType
-val = list.childNodes[1].nodeName
-
-val = list.children
-val = list.children[1].children
-
-val = list.firstChild
-val = list.firstElementChild
-
-val = list.lastChild
-val = list.lastElementChild
-
-val = list.childElementCount
-
-val = listItem.parentNode
-val = listItem.parentElement
-val = listItem.parentElement.parentElement
-
-val = listItem.nextSibling
-val = listItem.nextElementSibling   
-val = listItem.nextElementSibling.nextElementSibling   
-
-console.log(val)
+console.log(li)
