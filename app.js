a96530dc44256f2b1cafba4dda7492c3098462ea
Ronald Andero Õisman <ronald-andero.oisman@khk.ee>
2022-09-26 23:24:10 -0700

    DOM selectors for single and multiple elements

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 let val;
 
-val = document
-val = document.all[6]
-val = document.all.length
+val = document.getElementById('task')
+val = document.getElementById('task').id
+val = document.getElementById('task').className
 
-val = document.head
-val = document.body
+const taskTitle = document.getElementById('task-title')
 
-val = document.doctype
-val = document.domain
-val = document.URL
-val = document.characterSet
-val = document.contentType
+// style
+taskTitle.style.background = '#333'
+taskTitle.style.color = '#fff'
+taskTitle.style.padding = '5px'
+// taskTitle.style.display = 'none'
 
-val = document.forms
-val = document.forms[0].method
-val = document.forms[0].id
 
-val = document.links[1].classList[1]
+// content
+taskTitle.textContent = 'Define tasks'
+taskTitle.innerText = 'My favorite tasks'
+taskTitle.innerHTML = '<b style="color: red">My Tasks</b>'
 
-val = document.scripts[1].getAttribute('src')
+val = document.querySelector('#task-title')
+val = document.querySelector('.card-title')
+val = document.querySelector('ul')
+
+// const task = document.querySelector('li')
+// task.style.color = 'green'
+
+val = document.querySelector('li:last-child')
+// val = document.querySelector('li:nth-child(odd)').style.background = '#ccc'
+// val = document.querySelector('li:nth-child(even)').style.background = '#f4f4f4'
+
+// multiply elements
+val = document.querySelectorAll('li')
+oddLi = document.querySelectorAll('li:nth-child(odd)')
+
+oddLi.forEach(li => {
+    li.style.background = '#ccc'
+});
+
 console.log(val)
